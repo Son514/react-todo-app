d50ea4587741159532ee3eff8f51172e56a9c7ef
mjesplana1 <mjesplana@realhomes.ph>
2025-11-13 04:54:17 +0800

    Page Grid Layout UI   - Implemented CSS Grid for main layout structure

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import styles from "./mainlayout.module.css";
 
 const MainLayout = ({ children }) => {
-  return <div>{children || <Outlet />}</div>;
+  return (
+    <div>
+      <div className={styles["gridcontainer"]}>
+        <header className={styles["gridcontainer__header"]}>Header</header>
+        <aside className={styles["gridcontainer__sidebar"]}>Sidebar</aside>
+        <main className={styles["gridcontainer__main"]}>
+          {children || <Outlet />}
+          <h1>Content</h1>
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
